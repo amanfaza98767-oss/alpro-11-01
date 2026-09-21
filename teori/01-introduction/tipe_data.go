d9722bdcd18b128fc6c14a0013b9ef81a05fa83f
amanfaza98767-oss <amanfaza98767@gmail.com>
2026-09-21 13:23:46 +0700

    initial commit

---
+++ teori/01-introduction/tipe_data.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main() {
+	var umur int8
+	var anakke int8
+	var saldo int64
+
+	umur = 19
+	anakke = 1
+	saldo = 1000000000
+
+	fmt.Println("Umur saya adalah", umur, "tahun")
+	fmt.Println("Saya anak ke-", anakke)
+	fmt.Println("Saldo saya adalah Rp", saldo)
+
+}
